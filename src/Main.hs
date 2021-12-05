35a459c7fc21e161036c44b70ef5403e2654cff8
Sridhar Ratnakumar <srid@srid.ca>
2021-12-05 14:44:22 -0500

    shower model

--- src/Main.hs
+++ src/Main.hs
@@ -11,33 +11,34 @@ import qualified Ema.CLI
 import qualified Ema.CLI as CLI
 import qualified Ema.Helper.Tailwind as Tailwind
 import qualified HaskellPage.Source.Reddit as Reddit
+import qualified Shower
 import Text.Blaze.Html5 ((!))
 import qualified Text.Blaze.Html5 as H
 import qualified Text.Blaze.Html5.Attributes as A
 
 data Route
   = Index
-  | About
   deriving (Show, Enum, Bounded)
 
-newtype Model = Model {unModel :: Text}
+data Model = Model
+  { modelReddit :: Reddit.SubmissionListing
+  }
+  deriving (Eq, Show)
 
 instance Ema Model Route where
   encodeRoute _model =
     \case
       Index -> "index.html"
-      About -> "about.html"
   decodeRoute _model = \case
     "index.html" -> Just Index
-    "about.html" -> Just About
     _ -> Nothing
 
 main :: IO ()
 main = do
   Ema.runEma (\act m -> Ema.AssetGenerated Ema.Html . render act m) $ \act model -> do
-    LVar.set model $ Model "Hello World. "
     when (act == CLI.Run) $ do
-      _stuff <- liftIO Reddit.get
+      posts <- liftIO Reddit.get
+      LVar.set model $ Model posts
       putStrLn "Got stuff"
       liftIO $ threadDelay maxBound
 
@@ -45,15 +46,10 @@ render :: Ema.CLI.Action -> Model -> Route -> LByteString
 render emaAction model r =
   Tailwind.layout emaAction (H.title "Basic site" >> H.base ! A.href "/") $
     H.div ! A.class_ "container mx-auto" $ do
-      H.div ! A.class_ "mt-8 p-2 text-center" $ do
+      H.div ! A.class_ "mt-8 p-2" $ do
         case r of
           Index -> do
-            H.toHtml (unModel model)
-            "You are on the index page. "
-            routeElem About "Go to About"
-          About -> do
-            "You are on the about page. "
-            routeElem Index "Go to Index"
+            H.pre ! A.class_ "overflow-scroll text-xs" $ H.toHtml (Shower.shower model)
   where
     routeElem r' w =
       H.a ! A.class_ "text-red-500 hover:underline" ! routeHref r' $ w
