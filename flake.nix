0d4d0e69de73583263172acda7d8335cfc30701b
Sridhar Ratnakumar <srid@srid.ca>
2021-12-05 20:03:46 -0500

    move to toplevel

--- flake.nix
+++ flake.nix
@@ -13,6 +13,20 @@
     hercules-ci-effects.url = "github:hercules-ci/hercules-ci-effects";
   };
   outputs = inputs@{ self, nixpkgs, flake-utils, ... }:
+    let
+      effects = (import inputs.nixpkgs {
+        overlays = [
+          inputs.hercules-ci-effects
+        ];
+      }).effects;
+    in
+    {
+      helloEffect = effects.mkEffect {
+        effectScript = ''
+          echo Hello World
+        '';
+      };
+    } //
     flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" "aarch64-darwin" ] (system:
       let
         name = "haskellpage";
@@ -70,11 +84,6 @@
                   pkgs.nixpkgs-fmt
                 ]);
           };
-        effects = (import inputs.nixpkgs {
-          overlays = [
-            inputs.hercules-ci-effects
-          ];
-        }).effects;
       in
       {
         # Used by `nix build` & `nix run`
@@ -83,10 +92,5 @@
         # Used by `nix develop`
         devShell = project true;
 
-        helloEffect = effects.mkEffect {
-          effectScript = ''
-            echo Hello World
-          '';
-        };
       });
 }
